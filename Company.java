acc73e0f4d05fb29a301389e7737344d3beba69b
JoudAlharbi <joudacs06@gmail.com>
2024-11-22 18:56:34 +0300

    Company.java

---
+++ Company.java
@@ -0,0 +1,14 @@
+package ja7.airplaneticketsreservation;
+
+public class Company {
+
+    @Override
+    public String toString() {
+        return "who are we?\nwe are Saudi domestic aviation\nBook your flight with our Saudi Airlines today to your dream destination."
+                + " Whether you are looking to book a weekend in the city, a family summer vacation, or go out on a solo adventure, you"
+                + " can find trips to all cities of the Kingdom.\nThis is our first branch and our first experience. We aspire to be the"
+                + " best and to multiply our branches to become local and international\nOur services:\nOur services are varied and useful"
+                + " to the customer, which are: special seats for people with special needs with specialists who help them, flight classes,"
+                + " basic meals with children’s meals that include reservation.";
+    }
+}
